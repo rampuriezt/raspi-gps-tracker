a561bc572d332625eff5cd4b4ff3e60257da94ef
rampuriezt <rampuriezt@gmail.com>
2014-07-20 15:29:31 +0700

    Update gps-tcp-client.py

--- device-client/gps-tcp-client.py
+++ device-client/gps-tcp-client.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python
+
 from twisted.internet import reactor, protocol
+from rc4 import encrypt
+
+key = "1234567890"
+
 
 class EchoClient(protocol.Protocol):
    def connectionMade(self):
-       self.transport.write("Hello, world!")
+       #self.transport.write("Hello, world!")
+       gpsdata = "$GPGGA,184353.07,1929.045,S,02410.506,E,1,04,2.6,100.00,M,-33.9,M,,0000*6D"
+       enc = encrypt(gpsdata, key)
+       self.transport.write(enc)
+       #self.transport.write(data)
 
    def dataReceived(self, data):
        print "Server said:", data
